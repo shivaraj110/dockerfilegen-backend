801c40245cf05c5f8750a89af920aba8af784eb4
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-07-16 08:45:29 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 configDotenv({ quiet: true })
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (res: Response) => {
 	res.send('Hello World!')
 })
 
